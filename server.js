f0ab6ecf5aea7e81a7c7187e6aeac3d510c0780c
Dylan <user@users-MBP-2.broadband>
2022-03-13 15:04:17 +0000

    adding HTML request for index.html

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.listen(PORT, () => {
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Static folder for express
 app.use(express.static("public"));
@@ -30,6 +30,10 @@ app.use(express.static("public"));
 // HTML Requests
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 })
 
 
