31b4a0321905f5cb54fff9e15b84f70993d18722
Dylan <user@users-MBP-2.broadband>
2022-03-19 16:21:01 +0000

    Updating heroku

--- server.js
+++ server.js
@@ -1,69 +1,79 @@
 const express = require("express");
-const uuid = require("uuid");
 const path = require("path");
-
+const uuid = require("uuid");
 
 let noteStore = require("./db/db.json");
 const fs = require("fs");
 
-// initialising express
+// Starting express
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 
-// listen for sever
+// listening for server on Heroku port or 3001 locally
 app.listen(PORT, () => {
-  console.log(`
+    console.log(`
   
-     ______________________________________
-    |                                      |
-    |    Server started on port ${PORT}    |
-    |______________________________________|
-  `);
+    ______________________________________
+   |                                      |
+   |    Server started on port ${PORT}    |
+   |______________________________________|
+ `);
 });
 
-// Middleware
+// Tool middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-// Static folder for express
+// set a static folder for express
 app.use(express.static("public"));
 
-// HTML Requests
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
-})
-
+// HTML REQUESTS
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-})
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-// Configuration of the API Routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
-// Retreiving all inputted notes
+// API ROUTES
+
+// Get all availble notes from the store
 app.get("/api/notes", (req, res) => {
-    res.json(noteStore);
+  res.json(noteStore);
 });
 
-// User creates a note
+// Creates  a new note
 app.post("/api/notes", (req, res) => {
-    let userNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuid.v4(),
-};
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuid.v4(),
+  };
 
-    noteStore.push(newNote);
-    res.json(userNote);
+  noteStore.push(newNote);
 
-// write user note to database
-    fs.writeFile("db/db.json", JSON.stringify(userNote), (error) => {
-        if (error) throw error;
-        return true;
-    });
+  fs.writeFile("db/db.json", JSON.stringify(noteStore), (err) => {
+    if (err) throw err;
+    console.log("Database error has occured");
+  });
+
+  res.json(noteStore);
+
 });
 
+// Deleting a note
+app.delete("api/notes/:id", (req,res) => {
+    let { id } = req.params;
+    let findNote = noteStore.findIndex((note) => note.id !== id);
+    let delNote = noteStore.splice(findNote, 1);
+    res.send(delNote);
 
+    fs.writeFile("db/db.json", JSON.stringify(noteStore), (err) => {
+        if (err) throw err;
+        console.log("Database error has occured");
+      });
 
-
-
-
+      app.listen(PORT, () => 
+      console.log(`App is available at http://localhost:${PORT}`)); 
+})
